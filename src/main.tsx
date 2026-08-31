88539a1132632813b0c689cf8cd8c907a3a43674
qozm515 <46486213+qozm515@users.noreply.github.com>
2026-08-31 10:45:14 +0900

    Load responsive language and currency selector styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
 import { CreatorContact } from "./app/CreatorContact";
+import "./app/LanguagePreferences.css";
 import { PwaVersionStatus } from "./app/PwaVersionStatus";
 
 const rootElement = document.getElementById("root");
